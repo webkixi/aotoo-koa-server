c0a135189b146756892db2ec324a55efb655d36d
yc <kixi@163.com>
2017-09-06 14:20:16 +0800

    fkp内核方法template使用art-template

--- build/fkpcore/base/template/index.js
+++ build/fkpcore/base/template/index.js
@@ -8,31 +8,47 @@ exports.default = function (fkp) {
   return index;
 };
 
+var template = require('art-template');
+
+// 压缩页面的css/js
+// template.defaults.minimize = true;
+// template.defaults.htmlMinifierOptions = {
+//   collapseWhitespace: true,
+//   minifyCSS: true,
+//   minifyJS: true,
+//   // 运行时自动合并：rules.map(rule => rule.test)
+//   ignoreCustomFragments: []
+// };
+
 function index(fkp, raw) {
   var data = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-  var type = arguments[3];
+  var options = arguments[3];
 
   if (raw) {
-    var tempSet = {
-      1: {
-        evaluate: /{{([\s\S]+?)}}/g,
-        interpolate: /{{=([\s\S]+?)}}/g,
-        escape: /{{-([\s\S]+?)}}/g
-      },
+    // const tempSet = {
+    //   1: {
+    //     evaluate:    /{{([\s\S]+?)}}/g,
+    //     interpolate: /{{=([\s\S]+?)}}/g,
+    //     escape:      /{{-([\s\S]+?)}}/g
+    //   },
 
-      2: {
-        evaluate: /{{{([\s\S]+?)}}}/g,
-        interpolate: /{{{=([\s\S]+?)}}}/g,
-        escape: /{{{-([\s\S]+?)}}}/g
-      }
-    };
+    //   2: {
+    //     evaluate:    /{{{([\s\S]+?)}}}/g,
+    //     interpolate: /{{{=([\s\S]+?)}}}/g,
+    //     escape:      /{{{-([\s\S]+?)}}}/g
+    //   }
+    // }
 
-    if (typeof type == 'number') {
-      _.templateSettings = tempSet[type];
-    }
+    // if (typeof type == 'number') {
+    //   _.templateSettings = tempSet[type]
+    // }
 
-    var compiled = _.template(raw);
-    return compiled(data);
+    // const compiled = _.template(raw)
+    // return compiled(data)
+
+
+    // 将模板源代码编译成函数
+    return template.render(raw, data, options);
   }
 }
 //# sourceMappingURL=../../../maps/fkpcore/base/template/index.js.map
--- src/fkpcore/base/template/index.js
+++ src/fkpcore/base/template/index.js
@@ -1,25 +1,42 @@
-function index(fkp, raw, data={}, type) {
+const template = require('art-template');
+
+// 压缩页面的css/js
+// template.defaults.minimize = true;
+// template.defaults.htmlMinifierOptions = {
+//   collapseWhitespace: true,
+//   minifyCSS: true,
+//   minifyJS: true,
+//   // 运行时自动合并：rules.map(rule => rule.test)
+//   ignoreCustomFragments: []
+// };
+
+function index(fkp, raw, data={}, options) {
   if (raw) {
-    const tempSet = {
-      1: {
-        evaluate:    /{{([\s\S]+?)}}/g,
-        interpolate: /{{=([\s\S]+?)}}/g,
-        escape:      /{{-([\s\S]+?)}}/g
-      },
+    // const tempSet = {
+    //   1: {
+    //     evaluate:    /{{([\s\S]+?)}}/g,
+    //     interpolate: /{{=([\s\S]+?)}}/g,
+    //     escape:      /{{-([\s\S]+?)}}/g
+    //   },
 
-      2: {
-        evaluate:    /{{{([\s\S]+?)}}}/g,
-        interpolate: /{{{=([\s\S]+?)}}}/g,
-        escape:      /{{{-([\s\S]+?)}}}/g
-      }
-    }
+    //   2: {
+    //     evaluate:    /{{{([\s\S]+?)}}}/g,
+    //     interpolate: /{{{=([\s\S]+?)}}}/g,
+    //     escape:      /{{{-([\s\S]+?)}}}/g
+    //   }
+    // }
 
-    if (typeof type == 'number') {
-      _.templateSettings = tempSet[type]
-    }
+    // if (typeof type == 'number') {
+    //   _.templateSettings = tempSet[type]
+    // }
 
-    const compiled = _.template(raw)
-    return compiled(data)
+    // const compiled = _.template(raw)
+    // return compiled(data)
+
+
+    // 将模板源代码编译成函数
+    return template.render(raw, data, options);
+
   }
 }
 
